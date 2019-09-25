16a9d908325f75498e1de690f77e74bd2bc53202
tschedin <toms@tempworks.com>
2019-09-25 11:49:07 -0500

    Reverting out of the to-do list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import List from './List';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: '',
-      items: []
-    };
-  }
-
-  onChange = (event) => {
-    this.setState({ term: event.target.value });
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      term: '',
-      items: [...this.state.items, this.state.term]
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <form className="App" onSubmit={this.onSubmit}>
-          <input value={this.state.term} onChange={this.onChange} />
-          <button>Submit</button>
-        </form>
-        <List items={this.state.items} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          LETS GO VIKES!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.vikings.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Official Website
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
