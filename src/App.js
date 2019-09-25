ec98ad29791afa64d5612117f0455ed5540ce28a
tschedin <toms@tempworks.com>
2019-09-25 09:56:20 -0500

    Jumping to the to-do list for practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          LETS GO VIKES!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.vikings.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Official Website
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
-
-export default App;
