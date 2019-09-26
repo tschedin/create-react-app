a08ef3c7baf8af067f7a293ace39b0ded6939b27
tschedin <toms@tempworks.com>
2019-09-26 16:23:53 -0500

    define the items property

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,11 +4,15 @@ class TodoList extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            items: []
+        };
+
         this.addItem = this.addItem.bind(this);
     }
 
     addItem(e) {
-        
+
     }
     render() {
         return (
